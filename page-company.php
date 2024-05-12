4e2b0e0b752dea06431f4502523ac0260dbd942a
木下匠 <takumin4260@gmail.com>
2024-05-12 23:00:56 +0900

    company作成

---
+++ page-company.php
@@ -0,0 +1,74 @@
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/company.css">
+<main>
+  <div class="company_container">
+    <div class="title_wrap">
+      <h2 class="f-50 block tc mb-20">Company</h2>
+      <p class="f-16 block tc">会社案内</p>
+    </div>
+    <table class="company_table">
+      <tbody>
+        <tr>
+          <th class="f-16">会社名</th>
+          <td class="f-16">株式会社高橋建築</td>
+        </tr>
+        <tr>
+          <th class="f-16">代表取締役</th>
+          <td class="f-16">高橋 藤生</td>
+        </tr>
+        <tr>
+          <th class="f-16">所在地</th>
+          <td class="f-16">松山市古川西3丁目1-1</td>
+        </tr>
+        <tr>
+          <th class="f-16">TEL</th>
+          <td class="f-16">0596-22-9467</td>
+        </tr>
+        <tr>
+          <th class="f-16">FAX</th>
+          <td class="f-16">0596-22-9468</td>
+        </tr>
+        <tr>
+          <th class="f-16">営業時間</th>
+          <td class="f-16">9:00 - 18:00（土日定休）</td>
+        </tr>
+        <tr>
+          <th class="f-16">創業</th>
+          <td class="f-16">明治39年7月8日</td>
+        </tr>
+        <tr>
+          <th class="f-16">設立</th>
+          <td class="f-16">平成2年1月</td>
+        </tr>
+        <tr>
+          <th class="f-16">建築業許可</th>
+          <td class="f-16">三重県知事許可 (特-2)8621号</td>
+        </tr>
+        <tr>
+          <th class="f-16">許可番号</th>
+          <td class="f-16">
+            <span class="block lh1.75">
+              建設業許可番号 三重県知事許可 (特-2)8621号<br>
+              建築士事務所登録番号 一級建築士事務所 三重県知事登録 第1-2316号
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <th class="f-16">事業内容</th>
+          <td class="f-16">
+            <span class="block lh1.75">
+              １．建設工業事業　一般住宅・輸入住宅・店舗・マンション・官公庁 他<br>
+              ２．土地開発事業　住宅分譲 他<br>
+              ３．不動産事業　販売・仲介
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <th class="f-16">宅地建物取引業</th>
+          <td class="f-16">三重県知事(7)第2316号</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</main>
+<?php get_footer(); ?>
